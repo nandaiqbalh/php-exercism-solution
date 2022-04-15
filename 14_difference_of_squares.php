1c17761b325f16bea2346e14f9ac589a9ebedc37
nandaiqbalh <nandaiqbalhanafii@gmail.com>
2022-04-15 10:49:23 +0700

    Difference of Squares

---
+++ 14_difference_of_squares.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+function squareOfSum(int $limit) {
+    $sum = 0;
+    foreach (range(0, $limit) as $number) {
+        $sum += $number;
+    }
+    return pow($sum, 2);
+}
+function sumOfSquares(int $limit) {
+    $sum = 0;
+    foreach (range(0, $limit) as $number) {
+        $sum += pow($number, 2);
+    }
+    return $sum;
+}
+function difference(int $limit) {
+    return squareOfSum($limit) - sumOfSquares($limit);
+}
+ ?>
